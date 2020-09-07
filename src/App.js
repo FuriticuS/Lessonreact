cd078a6b9d9d7aa2981e0c17b39498c580eb7eb3
denist <Fur1ouS@ya.ru>
2020-09-07 18:12:45 +0300

    lazy load pages on app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+
 import {Route, withRouter} from "react-router-dom";
-import Navigation from "./components/navigation/Navigation";
-import News from "./components/news/News";
-import Music from "./components/music/Music";
-import Settings from "./components/settings/Settings";
-import DialogsContainer from "./components/dialogs/DialogsContainer";
-import UsersContainer from "./components/users/UsersContainer";
-import ProfileContainer from "./components/profile/ProfileContainer";
-import HeaderContainer from "./components/header/HeaderContainer";
-import Login from "./components/login/Login";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/reducer/app-reducer";
+
+import Navigation from "./components/navigation/Navigation";
+import Settings from "./components/settings/Settings";
+import HeaderContainer from "./components/header/HeaderContainer";
+import Login from "./components/login/Login";
 import Preloader from "./components/preloader/Preloader";
 
 import './css/Reset-styles.css';
 import './css/App.css';
+
+// lazy load - ленивая загрузка, компоненты подгружаются когда надо
+const DialogsContainer = React.lazy(() => import("./components/dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/profile/ProfileContainer"));
+const UsersContainer = React.lazy(() => import("./components/users/UsersContainer"));
+const News = React.lazy(() => import("./components/news/News"));
+const Music = React.lazy(() => import("./components/music/Music"));
 
 class App extends React.Component {
 
@@ -28,7 +32,7 @@ class App extends React.Component {
 
         // если мы не авторизированы верни прелоадер
         if (!this.props.initialized) {
-            return <Preloader />
+            return <Preloader/>
         }
 
         return (
@@ -42,15 +46,32 @@ class App extends React.Component {
                 <div className="content">
 
                     {/*данные находятся в state.js*/}
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>
+                    <Route path='/profile/:userId?' render={() =>
+                        <Suspense fallback={<Preloader />}>
+                            <ProfileContainer/>
+                        </Suspense>
                     }/>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>
+                    <Route path='/dialogs' render={() =>
+                        <Suspense fallback={<Preloader />}>
+                            <DialogsContainer/>
+                        </Suspense>
                     }/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/news' render={() =>
+                        <Suspense fallback={<Preloader />}>
+                            <News/>
+                        </Suspense>}/>
+
+                    <Route path='/music' render={() =>
+                        <Suspense fallback={<Preloader />}>
+                            <Music/>
+                        </Suspense>}/>
 
                     {/*можно вызвать как имя функции*/}
-                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/users' render={() =>
+                        <Suspense fallback={<Preloader />}>
+                            <UsersContainer/>
+                        </Suspense>
+                    }/>
 
                     {/*можно вызвать как имя функции*/}
                     <Route path='/settings' render={() => <Settings/>}/>
